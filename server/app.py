7ed8fe1c6455141971251226bf17322f36515371
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2023-09-12 23:46:33 -0400

    done

--- server/app.py
+++ server/app.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
+
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
